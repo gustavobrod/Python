60f3cce0e313364855856715b7799483001933b1
gustavobrod <gustavo.brod@outlook.com>
2021-12-11 21:27:14 -0300

    forca02

--- parte 1 - jogos/forca.py
+++ parte 1 - jogos/forca.py
@@ -10,7 +10,14 @@ def jogar():
     acertou = False
 
     while(not enforcou and not acertou):
-        print("jogando....")
+
+        chute = input("Qual a letra? ")
+
+        index = 0
+        for letra in palavra_secreta:
+            if(chute == letra):
+                print("Encontrei a letra {} na posição {}".format(letra, index))
+            index = index + 1
 
 
     print("\n################################")
