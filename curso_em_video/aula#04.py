983735b254dd8d7335848ba1e3c93645dfbefaa6
Gustavo Lima Brod <gustavo.brod@outlook.com>
2022-11-24 23:25:38 -0300

    aula#24112022

---
+++ curso_em_video/aula#04.py
@@ -0,0 +1,27 @@
+#print('Hola, mundo!')
+#print(7+4)
+#print('hola',4)
+
+#nome='Gustavo' 
+#idade=41
+#peso=80.2
+#print(nome,idade,peso)
+
+#nome=input('Qual seu nome? ')
+#idade=input('Quantos anos você tem? ')
+#peso=input('Qua seu peso? ')
+#print(nome,idade,peso)
+
+#nome=input('Qual seu nome? ')
+#print('Olá',nome,'! Prazer em te conhecer! ')
+
+#dia=input('Dia = ')
+#mes=input('Mês = ')
+#ano=input('Ano = ')
+#print('Você nasceu no',dia,'de',mes,'de',ano,'. Correto?')
+
+n1=int(input('Primeiro número '))
+n2=int(input('Segundo número '))
+soma=n1+n2
+print('A soma é',soma)
+
